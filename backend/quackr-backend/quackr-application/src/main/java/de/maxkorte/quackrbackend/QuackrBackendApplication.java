7f9c5a094ab795e9c89147cd153e0e8edf5f303d
Maximilian Korte <maximilian.korte@adesso.de>
2023-04-29 14:30:18 +0200

    Add Spring Boot configuration

---
+++ backend/quackr-backend/quackr-application/src/main/java/de/maxkorte/quackrbackend/QuackrBackendApplication.java
@@ -0,0 +1,13 @@
+package de.maxkorte.quackrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuackrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuackrBackendApplication.class, args);
+	}
+
+}

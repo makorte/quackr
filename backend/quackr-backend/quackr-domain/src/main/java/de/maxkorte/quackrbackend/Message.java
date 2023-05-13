42940aa96d867cb4be3fcf11a3ddd1b256908df8
Maximilian Korte <maximilian.korte@adesso.de>
2023-05-13 10:29:45 +0200

    Convert records to classes

--- backend/quackr-backend/quackr-domain/src/main/java/de/maxkorte/quackrbackend/Message.java
+++ backend/quackr-backend/quackr-domain/src/main/java/de/maxkorte/quackrbackend/Message.java
@@ -1,6 +1,58 @@
 package de.maxkorte.quackrbackend;
 
-import java.util.Date;
+public class Message {
+    private Long id;
+    private String createdAt;
+    private String title;
+    private String body;
+    private User user;
 
-public record Message (Long id, String createdAt, String title, String body, User user) {
+    public Message(Long id, String createdAt, String title, String body, User user) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
+
--- backend/quackr-backend/quackr-domain/src/main/java/de/maxkorte/quackrbackend/User.java
+++ backend/quackr-backend/quackr-domain/src/main/java/de/maxkorte/quackrbackend/User.java
@@ -1,4 +1,27 @@
 package de.maxkorte.quackrbackend;
 
-public record User(Long id, String username) {
+public class User {
+    private Long id;
+    private String username;
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
